9f505a3509a3e1365c07bcb1668af35a6244b257
jiyoung <positive246@naver.com>
2024-01-17 23:56:22 +0900

    React 1~5 section

---
+++ react-start/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// import Library from './chapter_03/Library';
+// import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
+
+// chapter 3
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Library />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// chapter 4
+//setInterval(() => {
+//  ReactDOM.render(
+//    <React.StrictMode>
+//      <Clock />
+//    </React.StrictMode>,
+//    document.getElementById('root')
+//  );
+//}, 1000);
+
+// chapter 5
+ReactDOM.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
